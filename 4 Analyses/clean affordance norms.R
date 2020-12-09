bc2b43171a5ceee2ac5b682171366799656788e4
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2020-12-09 12:48:54 -0600

    finish up data cleaning

--- 4 Analyses/clean affordance norms.R
+++ 4 Analyses/clean affordance norms.R
@@ -99,16 +99,47 @@ dat$affordance_lemma = lemmatize_strings(dat$affordance_corrected)
 ##Treetagger would probably be better, but I can't get it to work, will try and get back to this though.
 ##Treetagger also pulls part of speech, which would be nice to have.
 
-##what about udpipe?
+##what about using udpipe?
+
 #Example
-x <- c(doc_a = "In our last meeting, someone said that we are meeting again tomorrow",
-       doc_b = "It's better to be good at being the best")
-anno <- udpipe(x, "english")
-anno[, c("doc_id", "sentence_id", "token", "lemma", "upos")]
+#x = c(doc_a = "In our last meeting, someone said that we are meeting again tomorrow",
+#       doc_b = "It's better to be good at being the best")
+#anno = udpipe(x, "english")
+#anno[, c("doc_id", "sentence_id", "token", "lemma", "upos")]
 
 ##Oooh, I like this as an alternative to treetagger.
 #does it work on vectors?
-#lematized = udpipe(dat$affordance_corrected, "english")
+#lemmatized = udpipe(dat$affordance_corrected, "english")
 
 #It does, but there are a few issues. Mainly it ended up with extra tokens somehow
 #Get only the overlapping tokens.
+
+#Is there a way to do this after things have already been tokenized?
+#en = udpipe_download_model(language = "english") #Download English model to working directory (should only need to do this once)
+udmodel_english <- udpipe_load_model(file = "english-ewt-ud-2.5-191206.udpipe") #load the model
+
+#temp = udpipe_annotate(udmodel_english, x = dat$affordance_corrected)
+#temp = as.data.frame(temp)
+temp = udpipe_annotate(udmodel_english, x = dat$affordance_corrected, tokenizer = "vertical")
+temp = as.data.frame(temp)
+
+dat$affordance_lemma = temp$lemma
+dat$POS = temp$upos
+
+#write to .csv -- good idea to open file in excel and double check that lemmas look good
+#write.csv(dat, file = "dat_lemmatized.csv", row.names = F)
+
+####Stop words####
+##Read back in the corrected lemmatized data
+dat = read.csv("dat_lemmatized.csv", stringsAsFactors = F)
+
+#Remove puncuation and stopwords from lemmas
+dat$affordance_lemma = gsub("\\-", " ", dat$affordance_lemma)
+dat$affordance_lemma = gsub("^$|\002", NA, trimws(dat$affordance_lemma))
+
+dat.nostop = dat %>% filter(!grepl("[[:punct:]]", affordance_lemma)) %>%
+  filter(!affordance_lemma %in% stopwords(language = "en", source = "snowball")) %>% 
+  filter(!is.na(affordance_lemma))
+
+##Write no stop data to .csv
+#write.csv(dat.nostop, file = "cleaned_output.csv", row.names = F)

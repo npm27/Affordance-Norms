eb9511a3253c5da7c617e822218c2444b0d463a9
npm27 <nick.maxwell61@yahoo.com>
2022-03-17 17:01:34 -0500

    lemmatize and correct
    take one

--- 4 Analyses/R/Scripts/Standardize Responses.R
+++ 4 Analyses/R/Scripts/Standardize Responses.R
@@ -16,16 +16,51 @@ dat$Initial[dat$Initial == "this is a person. i hope you would do nothing else b
 
 dat = na.omit(dat)
 
-##Remove any final punctuation and white space
+##Remove any punctuation and white space
 dat$cleaned = gsub("[[:punct:]]", "", dat$Initial)
 
 ##Remove stop words
 no_stop = dat %>%
-  filter(!grepl("[[:punct:]]", cleaned)) %>% #Remove punctuation
+  filter(!grepl("[[:punct:]]", cleaned)) %>% #Remove any final punctuation
   filter(!cleaned %in% stopwords(language = "en", source = "snowball")) %>% #remove stopwords
   filter(!grepl("[[:digit:]]+", cleaned)) %>% #remove numbers
   filter(!is.na(cleaned))
 
 ##Now lemmatize
+#extract updated tokens
+tokens = unnest_tokens(tbl = no_stop, output = final_affordance, input = cleaned)
 
-##remove stops one more time
+#now remove stop words one more time
+no_stop_final = tokens %>%
+  filter(!final_affordance %in% stopwords(language = "en", source = "snowball")) %>% #remove stopwords
+  filter(!is.na(final_affordance ))
+
+#remove a couple of weird responses
+no_stop_final$final_affordance[no_stop_final$final_affordance == "aa"] = NA
+
+#remove a few other common words
+no_stop_final$final_affordance[no_stop_final$final_affordance == "able"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "can"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "thankyou"] = "thank"
+no_stop_final$final_affordance[no_stop_final$final_affordance == "youre"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "youre"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "wouldnt"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "shouldnt"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "couldnt"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "don't"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "he's"] = NA
+no_stop_final$final_affordance[no_stop_final$final_affordance == "it's"] = NA
+
+no_stop_final = na.omit(no_stop_final)
+
+#get the cue items
+cuelist = unique(no_stop_final$Stimuli.Cue)
+
+##okay, I think this does what I want.
+no_stop_final$affordance_lemma = lemmatize_strings(no_stop_final$final_affordance)
+
+#fix weird lemmas
+no_stop_final$affordance_lemma[no_stop_final$affordance_lemma == "spin-dry"] = "dry"
+
+##Write to file
+#write.csv(no_stop_final, file = "lemmatized.csv", row.names = F) #Spot check and clean in Excel

ff214d897189f342296d3fa1e10a00457e5ca69a
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2023-01-07 17:26:13 -0600

    clean Prolific Batch 1

---
+++ 3 Output/1 Raw Output/Prolific/combine raw.R
@@ -0,0 +1,23 @@
+####Combine .csv files into master datasheet####
+setwd("./Batch 1")
+
+#Get the files names
+files = list.files(pattern = "*.csv")
+
+#read everything in and merge into one dataframe.
+dat = do.call(rbind, lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+
+setwd("..")
+
+##remove instruction trials
+unique(dat$Procedure.Trial.Type)
+
+dat2 = subset(dat,
+              dat$Procedure.Trial.Type != "Instruct")
+
+##Remove practice trials
+dat2 = subset(dat2,
+              dat2$Procedure.Item < 3001)
+
+##Write to .csv
+#write.csv(dat2, file = "Merged_Prolific_Batch1.csv", row.names = F)

8b52bd1376114188ebb85ae3e5c85292ccaea05e
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2023-07-25 18:38:54 -0500

    Clean Prolific Batch 12

--- 3 Output/1 Raw Output/Prolific/combine raw.R
+++ 3 Output/1 Raw Output/Prolific/combine raw.R
@@ -1,5 +1,5 @@
 ####Combine .csv files into master datasheet####
-setwd("./Batch 11")
+setwd("./Batch 12")
 
 #Get the files names
 files = list.files(pattern = "*.csv")
@@ -20,4 +20,4 @@ dat2 = subset(dat2,
               dat2$Procedure.Item < 631)
 
 ##Write to .csv
-#write.csv(dat2, file = "Merged_Prolific_Batch11.csv", row.names = F)
+#write.csv(dat2, file = "Merged_Prolific_Batch12.csv", row.names = F)

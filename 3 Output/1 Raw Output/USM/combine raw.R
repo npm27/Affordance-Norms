53ed4dde202df96211ad48e7599cd0e7463faf66
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2023-09-17 18:42:44 -0500

    Clean USM Batch 7

--- 3 Output/1 Raw Output/USM/combine raw.R
+++ 3 Output/1 Raw Output/USM/combine raw.R
@@ -1,5 +1,5 @@
 ####Combine .csv files into master datasheet####
-setwd("./Raw Batch 6")
+setwd("./Raw Batch 7")
 
 #Get the files names
 files = list.files(pattern = "*.csv")
@@ -20,4 +20,4 @@ dat2 = subset(dat2,
               dat2$Procedure.Item < 631)
 
 ##Write to .csv
-#write.csv(dat2, file = "Merged_USM_Batch6.csv", row.names = F)
+#write.csv(dat2, file = "Merged_USM_Batch7.csv", row.names = F)
